b976b578b8f4cf5a52b681b10075bfef212ba276
godulam <maciek.godula@gmail.com>
2017-02-28 14:38:28 +0100

    for loop edit

--- SimpleForLoop/SimpleForLoop.java
+++ SimpleForLoop/SimpleForLoop.java
@@ -2,9 +2,14 @@ public class SimpleForLoop
 {
 	public static void main (String[] args)
 	{
-		for (int i=0 ; i<10 ; i++)
+		for (int i=1 ; i<10 ; i++)
 		{
-			System.out.println(i);
+			System.out.print(i + " multiplications ");
+			for  (int k=1 ; k<10 ; k++)
+			{
+				System.out.print(i * k + " ");
+			}
+			System.out.print("\n");
 		}
 
 	}
